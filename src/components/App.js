314193789678b90a91f5a25c6f0dfc83076d375b
jwc20 <choij2015@gmail.com>
2022-03-29 13:23:06 -0700

    Fetched database from the-one-api

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,71 @@
 import React from "react";
 import { useState, useEffect } from "react";
 function App() {
+  // function Search() {
+  //   // TODO
 
+  //   function SearchForm() {
+  //     // TODO
+  //   }
 
-  function Search() {
-    // TODO
-  
-    function SearchForm() {
-      // TODO
-    }
+  //   function Filter() {
+  //     // TODO
+  //   }
+  //   return (
+  //     <>
+  //       <SearchForm />
+  //       <Filter />
+  //     </>
+  //   );
+  // }
 
-    function Filter() {
-      // TODO
-    }
-    return (
-      <>
-        <SearchForm />
-        <Filter />
-      </>
-    );
-  }
+  // function CharacterContainer() {
+  //   //TODO
 
+  //   function Character() {
+  //     //TODO
+  //   }
 
-  function CharacterContainer() {
-    //TODO
-    
-    function Character() {
-      //TODO
-    }
+  //   return <Character />;
+  // }
 
-    return (
-      <Character />
-    )
-  }
+  const [character, setCharacter] = useState();
+
+  useEffect(() => {
+    const headers = {
+      Accept: "application/json",
+      Authorization: "Bearer xvi06TocPJvBmrQC4yZv",
+    };
+
+    const fetchData = async () => {
+      const rawQuotes = await fetch("https://the-one-api.dev/v2/quote", {
+        headers: headers,
+      });
+      const rawCharacters = await fetch(
+        "https://the-one-api.dev/v2/character",
+        { headers: headers }
+      );
+      const characters = await rawCharacters.json(); // All Characters
+      const character = characters.docs[0]; 
+      console.log(character)
+      setCharacter(character.name);
+    };
+
+    fetchData();
+  }, []);
+
 
   return (
     <div className="App">
       <h1>People of Middle-Earth</h1>
-      <Search />
-      <CharacterContainer /> 
+       {character}
+
+      {/*
+        <Search />
+        <CharacterContainer />
+      */}
+
+
     </div>
   );
 }
